36352d589dadba30a9e6c2b5369dc1747c6de029
Rian <brianalmeida998@gmail.com>
2022-07-29 15:48:05 -0300

    criando rotas e reorganizando estilo

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/estaticos/navbar/Navbar";
 import Footer from "./components/estaticos/footer/Footer";
 import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
-  );
+    </Router>
+  )
 }
 
 export default App;
--- blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -1,43 +1,48 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import './Navbar.css';
 
 function Navbar() {
     return (
         <>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <Box style={{ cursor: "pointer" }} >
+                    <Box className='cursor'>
                         <Typography variant="h5" color="inherit">
                             BlogPessoal
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Home
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Postagens
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Cadastrar Tema
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                Logout
-                            </Typography>
-                        </Box>
+                        <Link to='./login' className='text-decorator-none'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Logout
+                                </Typography>
+                            </Box>
+                        </Link>
+
                     </Box>
 
                 </Toolbar>
