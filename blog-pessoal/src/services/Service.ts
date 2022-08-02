7aa21d711fef8d39b006fab432d19225d1b695a0
Rian <brianalmeida998@gmail.com>
2022-08-02 12:59:27 -0300

    Implementação de segurança do projeto (Login | Cadastro)

---
+++ blog-pessoal/src/services/Service.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { url } from "inspector";
+
+export const api = axios.create({
+    baseURL: 'https://omeublogpessoal.herokuapp.com'
+})
+
+export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data)
+}
+
+export const login = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data.token)
+}
