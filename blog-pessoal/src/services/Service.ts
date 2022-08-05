098fab03963dafc47664a062e52dfb2eb0dcfa11
Rian <brianalmeida998@gmail.com>
2022-08-05 10:30:47 -0300

    implementando Tema e Postagem na Model, e adicionando função na Service

--- blog-pessoal/src/services/Service.ts
+++ blog-pessoal/src/services/Service.ts
@@ -5,12 +5,17 @@ export const api = axios.create({
     baseURL: 'https://omeublogpessoal.herokuapp.com'
 })
 
-export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data)
 }
 
-export const login = async(url: any, dados: any, setDado: any) => {
+export const login = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
+    setDado(resposta.data.token)
+}
+
+export const busca = async (url: any, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
     setDado(resposta.data.token)
 }
